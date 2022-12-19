9a17c11432f51c90294b74054bec12e607484d31
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-12-19 18:39:42 +0800

    added nodemon.

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,4 +3,5 @@ const getNotes = require("./notes");
 const msg = getNotes();
 console.log(msg);
 
-console.log(chalk.green("Success!"));
+console.log(chalk.bgGreen.bold("Success!"));
+console.log(chalk.red.inverse.bold("Error!"));
