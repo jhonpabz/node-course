f9d0b670bc4520314c4e97f7a0e7cd3f4cb6384b
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2022-12-20 23:35:43 +0800

    added yargs command.

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,4 +5,24 @@ const getNotes = require("./notes");
 // Customize yargs version
 yargs.version("1.1.0");
 
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function () {
+    console.log("Removing note");
+  },
+});
+
+// add, remove, read, list
+
 console.log(yargs.argv);
