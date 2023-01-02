953a3cc1d831d028799b9f942ba39e09d394689d
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-01-02 18:46:45 +0800

    argumanent parsing with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,8 +9,21 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: function () {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: ", argv.title);
+    console.log("Body: ", argv.body);
   },
 });
 
@@ -26,7 +39,7 @@ yargs.command({
 // Create list command
 yargs.command({
   command: "list",
-  describe: "list a note",
+  describe: "List your note",
   handler: function () {
     console.log("Listing note");
   },
@@ -41,6 +54,4 @@ yargs.command({
   },
 });
 
-// add, remove, read, list
-
-console.log(yargs.argv);
+yargs.parse();
