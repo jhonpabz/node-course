93b6b10d06c3c8fbe2aa6d0895b88ba444c24d18
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-01-03 11:22:03 +0800

    overwrite new data

--- playground/1-json.js
+++ playground/1-json.js
@@ -7,7 +7,17 @@ const fs = require("fs");
 // const bookJSON = JSON.stringify(book);
 // fs.writeFileSync("1-json.json", bookJSON);
 
+// const dataBuffer = fs.readFileSync("1-json.json");
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.author);
+
 const dataBuffer = fs.readFileSync("1-json.json");
 const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.author);
+const user = JSON.parse(dataJSON);
+
+user.name = "zxcv";
+user.age = 227;
+console.log(user, "user");
+const userJSON = JSON.stringify(user);
+fs.writeFileSync("1-json.json", userJSON);
