e87b09294b22fb1dab9fa62fcf5719b54e170b2b
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-01-04 14:39:54 +0800

    add and save notes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,35 @@
+const fs = require("fs");
 const getNotes = () => {
   return "Your notes...";
 };
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+  const notes = loadNotes();
+
+  notes.push({
+    title: title,
+    body: body,
+  });
+
+  saveNotes(notes);
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const data = fs.readFileSync("notes.json");
+    const dataJSON = data.toString();
+    return JSON.parse(dataJSON);
+  } catch (err) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
