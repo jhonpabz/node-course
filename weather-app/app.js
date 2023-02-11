2ff836c1f94c753ab6e666406aecd3ef9b27cf60
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-11 19:44:47 +0800

    Added positionstack api to convert city to geocode and print latitude and longitude.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,39 @@
 require('dotenv').config();
 const request = require('postman-request');
 
-const weatherApiKey = process.env.WEATHERSTACK_API_KEY;
-const url =
-  'http://api.weatherstack.com/current?access_key=' +
-  weatherApiKey +
-  '&query=New%20York';
+const WEATHER_KEY = process.env.WEATHERSTACK_API_KEY;
+const POSITIONS_KEY = process.env.POSITIONSTACK_API_KEY;
 
-request({ url: url, json: true }, (error, response) => {
-  const data = response.body.current;
+// const url =
+//   'http://api.weatherstack.com/current?access_key=' +
+//   WEATHER_KEY +
+//   '&query=New%20York';
 
-  const temp = data.temperature;
-  const feelsLikeTemp = data.feelslike;
-  const weatherDesc = data.weather_descriptions[0];
-  console.log(
-    weatherDesc +
-      '. It is currently ' +
-      temp +
-      ' degrees out. It feels like ' +
-      feelsLikeTemp +
-      ' degrees out.'
-  );
+// request({ url: url, json: true }, (error, response) => {
+//   const data = response.body.current;
+
+//   const temp = data.temperature;
+//   const feelsLikeTemp = data.feelslike;
+//   const weatherDesc = data.weather_descriptions[0];
+//   console.log(
+//     weatherDesc +
+//       '. It is currently ' +
+//       temp +
+//       ' degrees out. It feels like ' +
+//       feelsLikeTemp +
+//       ' degrees out.'
+//   );
+// });
+
+const geocodeURL =
+  'http://api.positionstack.com/v1/forward?access_key=' +
+  POSITIONS_KEY +
+  '&query=Los%20Angeles';
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const result = response.body.data;
+  // console.log(result, 'result');
+
+  console.log('Latitude: ', result[0].latitude);
+  console.log('Longitude: ', result[0].longitude);
 });
