564f0e354601b9c1586755e2f193b6ffa5d6f746
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-12 00:25:07 +0800

    Update weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,26 +4,26 @@ const request = require('postman-request');
 const WEATHER_KEY = process.env.WEATHERSTACK_API_KEY;
 const POSITIONS_KEY = process.env.POSITIONSTACK_API_KEY;
 
-// const url =
-//   'http://api.weatherstack.com/current?access_key=' +
-//   WEATHER_KEY +
-//   '&query=New%20York';
+const url =
+  'http://api.weatherstack.com/current?access_key=' +
+  WEATHER_KEY +
+  '&query=New%20York';
 
-// request({ url: url, json: true }, (error, response) => {
-//   const data = response.body.current;
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body.current;
 
-//   const temp = data.temperature;
-//   const feelsLikeTemp = data.feelslike;
-//   const weatherDesc = data.weather_descriptions[0];
-//   console.log(
-//     weatherDesc +
-//       '. It is currently ' +
-//       temp +
-//       ' degrees out. It feels like ' +
-//       feelsLikeTemp +
-//       ' degrees out.'
-//   );
-// });
+  const temp = data.temperature;
+  const feelsLikeTemp = data.feelslike;
+  const weatherDesc = data.weather_descriptions[0];
+  console.log(
+    weatherDesc +
+      '. It is currently ' +
+      temp +
+      ' degrees out. It feels like ' +
+      feelsLikeTemp +
+      ' degrees out.'
+  );
+});
 
 const geocodeURL =
   'http://api.positionstack.com/v1/forward?access_key=' +
@@ -32,8 +32,10 @@ const geocodeURL =
 
 request({ url: geocodeURL, json: true }, (error, response) => {
   const result = response.body.data;
-  // console.log(result, 'result');
 
-  console.log('Latitude: ', result[0].latitude);
-  console.log('Longitude: ', result[0].longitude);
+  const latitude = result[0].latitude;
+  const longitude = result[0].longitude;
+
+  console.log('Latitude: ', latitude);
+  console.log('Longitude: ', longitude);
 });
