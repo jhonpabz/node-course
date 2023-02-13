bfc995bb0c6659fe2efc4d74bdd1a4fc0b3dd4f0
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-14 07:31:18 +0800

    Finish challenge

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -1,23 +1,34 @@
-setTimeout(() => {
-  console.log('Two seconds are done');
-}, 2000);
+// setTimeout(() => {
+//   console.log('Two seconds are done');
+// }, 2000);
 
-const names = ['Jhon', 'Janin', 'Ja'];
-const shortNames = names.filter((name) => {
-  return name.length <= 4;
-});
+// const names = ['Jhon', 'Janin', 'Ja'];
+// const shortNames = names.filter((name) => {
+//   return name.length <= 4;
+// });
 
-const geocode = (address, callback) => {
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       latitude: 0,
+//       longitude: 0,
+//     };
+
+//     callback(data);
+//   }, 2000);
+// };
+
+// geocode('Urdaneta', (data) => {
+//   console.log(data);
+// });
+
+const add = (a, b, c) => {
   setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0,
-    };
-
-    callback(data);
+    const s = a + b;
+    c(s);
   }, 2000);
 };
 
-geocode('Urdaneta', (data) => {
-  console.log(data);
+add(1, 4, (sum) => {
+  console.log(sum);
 });
