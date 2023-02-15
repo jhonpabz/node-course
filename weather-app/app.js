b4bc1ac95d3fe2ca7d255d2631be9d8cc70cdc6d
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-15 22:33:33 +0800

    Added error handler.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,16 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Quezon', (error, data) => {
-  console.log('Error: ', error);
-  console.log('Geocode Data: ', data);
+geocode('Urdaneta', (error, data) => {
+  if (error) {
+    return console.log(error);
+  }
 
   forecast(data.latitude, data.longitude, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
     console.log('Error', error);
     console.log('Forecast Data: ', data);
   });
 });
-
-// forecast(15.980513, 120.45983, (error, data) => {
-//   console.log('Error', error);
-//   console.log('Data', data);
-// });
