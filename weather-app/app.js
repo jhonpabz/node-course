93311e6f7f388c75b3dd2a6f3051cbb1ae4c3979
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-15 22:26:14 +0800

    Chain geocode with forecast callback.

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,10 +3,15 @@ const forecast = require('./utils/forecast');
 
 geocode('Quezon', (error, data) => {
   console.log('Error: ', error);
-  console.log('Data: ', data);
+  console.log('Geocode Data: ', data);
+
+  forecast(data.latitude, data.longitude, (error, data) => {
+    console.log('Error', error);
+    console.log('Forecast Data: ', data);
+  });
 });
 
-forecast(15.980513, 120.45983, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+// forecast(15.980513, 120.45983, (error, data) => {
+//   console.log('Error', error);
+//   console.log('Data', data);
+// });
