83c4d2fa061a5d0949a42f20acdb3d251427b51a
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-20 19:24:15 +0800

    Serving up static assets.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 const port = 3000;
+const publicDirPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirPath));
 
 app.get('/', (req, res) => {
   res.send('<h1>zxcv</h1>');
