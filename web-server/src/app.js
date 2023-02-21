98794c6ee1e359a6871fca378c560e935b4f2a1a
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-21 18:47:03 +0800

    Can visit page without app.use but you will add .html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,22 +2,14 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = 3000;
-const publicDirPath = path.join(__dirname, '../public');
+const port = 8000;
+const publicDirPath = path.join(__dirname, '../public/');
 
 app.use(express.static(publicDirPath));
 
-app.use('/help', express.static(path.join(__dirname, '../public/help.html')));
+// app.use('/help', express.static(path.join(__dirname, '../public/help.html')));
 
-app.use('/about', express.static(path.join(__dirname, '../public/about.html')));
-
-app.get('/weather', (req, res) => {
-  res.send({
-    temp: '36 deg',
-    feelslike: '37 deg',
-    location: 'Quezon City',
-  });
-});
+// app.use('/about', express.static(path.join(__dirname, '../public/about.html')));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
