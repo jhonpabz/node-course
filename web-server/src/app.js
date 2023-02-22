68058492296041725d13efd5a635042d5bc2c3ef
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-22 18:49:43 +0800

    Render hbs page and remove the static page.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,8 +5,12 @@ const app = express();
 const port = 8000;
 const publicDirPath = path.join(__dirname, '../public/');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirPath));
 
+app.get('', (req, res) => {
+  res.render('index');
+});
 // app.use('/help', express.static(path.join(__dirname, '../public/help.html')));
 
 // app.use('/about', express.static(path.join(__dirname, '../public/about.html')));
