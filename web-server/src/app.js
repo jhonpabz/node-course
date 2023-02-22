82c88bbfbfe9296f7251715b970d4d847c36688c
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-22 22:33:34 +0800

    Added help hbs page.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -21,6 +21,14 @@ app.get('/about', (req, res) => {
     name: 'Jhon Pabz',
   });
 });
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  });
+});
 // app.use('/help', express.static(path.join(__dirname, '../public/help.html')));
 
 // app.use('/about', express.static(path.join(__dirname, '../public/about.html')));
