bc7050a14206457e49150b7bfb0292179ea6a32e
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-23 14:44:00 +0800

    Added comments.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,16 @@ const express = require('express');
 
 const app = express();
 const port = 8000;
+
+// Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public/');
 const viewsPath = path.join(__dirname, '../templates');
 
+// Setup handlebars engine views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
