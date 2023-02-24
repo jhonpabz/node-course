348c0d1adcc57455e6307e1df06347d460433978
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-24 10:46:31 +0800

    Added dynamic title to header.hbs.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -37,6 +37,7 @@ app.get('/help', (req, res) => {
     title: 'Help',
     message:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    name: 'Jhon Pabz',
   });
 });
 // app.use('/help', express.static(path.join(__dirname, '../public/help.html')));
