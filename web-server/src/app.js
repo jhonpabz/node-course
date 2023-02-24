4745fc768ce047c60d0f50447a3baa2b2cc88560
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-24 10:43:38 +0800

    Added hbs header template.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
-const port = 8000;
+const port = 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public/');
