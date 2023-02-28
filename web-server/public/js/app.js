0613c52cf8900d4c948434295ad4810784afe203
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-28 23:08:25 +0800

    Fetch weather api and log to client side.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,8 +1,17 @@
 // const { response } = require("express");
 console.log('Client side js file');
 
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+fetch('http://localhost:3000/weather?address=makati').then((response) => {
   response.json().then((data) => {
-    console.log(data);
+    console.log('weather', data);
   });
+  // .catch((err) => {
+  //   console.log('error', err);
+  // });
 });
