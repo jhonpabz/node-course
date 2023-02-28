59610ba0f81d91d39b275a939560baab942bb323
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-02-28 22:20:23 +0800

    Fetch data from client side.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,8 @@
+// const { response } = require("express");
 console.log('Client side js file');
+
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
