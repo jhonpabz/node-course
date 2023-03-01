15edd530585ef5f44ac699fd605b2a1b3a37ce14
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-01 22:03:22 +0800

    Added search form and log fetched search value.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,12 +7,22 @@ console.log('Client side js file');
 //   });
 // });
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log('Error: ', data.error);
-    } else {
-      console.log('weather', data);
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+
+  fetch('http://localhost:3000/weather?address=' + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log('Error: ', data.error);
+        } else {
+          console.log('weather', data);
+        }
+      });
     }
-  });
+  );
 });
